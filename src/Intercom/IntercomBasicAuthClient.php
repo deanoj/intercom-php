6bd0469185857a6082267b823692d565f2304902
Dean McGill <dean.mcgill@hpi.co.uk>
2014-10-30 15:47:52 +0000

    gitignore updated IntercomBasicAuthClient fixed for php5.3

--- src/Intercom/IntercomBasicAuthClient.php
+++ src/Intercom/IntercomBasicAuthClient.php
@@ -7,12 +7,12 @@ use Guzzle\Service\Client;
 class IntercomBasicAuthClient extends IntercomAbstractClient
 {
     /** @var array The required config variables for this type of client */
-    private static $required = [
+    private static $required = array(
         'app_id',
         'api_key',
         'headers',
         'service_description'
-    ];
+    );
 
     /**
      * Creates a basic auth client with the supplied configuration options
